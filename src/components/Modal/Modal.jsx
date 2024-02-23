9aad431c5499bafe941b027980bc44decc0e95d1
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-02-23 18:16:56 +0100

    Add documentation to modal component and update export

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,15 @@ import PropTypes from "prop-types";
 
 import styles from "./Modal.module.scss";
 
+/**
+ * Modal component that displays a message and provides a close button.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.message - The message to be displayed in the modal.
+ * @param {Function} props.onClose - The function to be called when the modal is closed.
+ * @returns {JSX.Element} The rendered Modal component.
+ */
 export const Modal = ({ message, onClose }) => (
 	<div className={styles.modal}>
 		<div className={styles.content}>
@@ -22,5 +31,3 @@ Modal.defaultProps = {
 	message: "",
 	onClose: () => {},
 };
-
-export default Modal;
