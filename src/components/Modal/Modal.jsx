737ab966e1a618877485c8905c0f8c7306ab79f0
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-02-24 11:54:41 +0100

    Add isOpen prop to Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,11 +8,12 @@ import styles from "./Modal.module.scss";
  * @component
  * @param {Object} props - The component props.
  * @param {React.ReactNode} props.message - The message to be displayed in the modal.
+ * @param {boolean} props.isOpen - Whether the modal is open or not.
  * @param {Function} props.onClose - The function to be called when the modal is closed.
  * @returns {JSX.Element} The rendered Modal component.
  */
-export const Modal = ({ message, onClose }) => (
-	<div className={styles.modal}>
+export const Modal = ({ message, isOpen, onClose }) => (
+	<div className={styles.modal} style={{ display: isOpen ? "block" : "none" }}>
 		<div className={styles.content}>
 			<button className={styles.close} onClick={onClose}>
 				X
@@ -24,10 +25,12 @@ export const Modal = ({ message, onClose }) => (
 
 Modal.propTypes = {
 	message: PropTypes.node.isRequired,
+	isOpen: PropTypes.bool.isRequired,
 	onClose: PropTypes.func.isRequired,
 };
 
 Modal.defaultProps = {
 	message: "",
+	isOpen: false,
 	onClose: () => {},
 };
